e15594aed489fe349babe8f67ca746a44f8e628a
AlbertZhuPersonal <66673048+AlbertZhuPersonal@users.noreply.github.com>
2020-06-12 23:54:02 -0400

    second anim

---
+++ littleanim.js
@@ -0,0 +1,22 @@
+var textWrapper = document.querySelector('.ml6 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+  target: '.ml6',
+  duration: 3500
+})
+  .add({
+    targets: '.ml6 .letter',
+    translateY: ["1.1em", 0],
+  opacity: 1,
+    translateZ: 0,
+    duration: 750,
+    delay: (el, i) => 50 * i
+  }).add({
+    targets: '.ml6',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000000
+  });
