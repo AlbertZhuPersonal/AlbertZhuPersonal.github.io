a178ea40359c1f39629e1eab48d509e2ac65f034
AlbertZhuPersonal <66673048+AlbertZhuPersonal@users.noreply.github.com>
2020-06-12 23:12:12 -0400

    update

--- line-anim.js
+++ line-anim.js
@@ -1,38 +1,38 @@
 anime.timeline({loop: true})
   .add({
-    targets: '.biganim .line',
+    targets: '.ml5 .line',
     opacity: [0.5,1],
     scaleX: [0, 1],
     easing: "easeInOutExpo",
     duration: 700
   }).add({
-    targets: '.biganim .line',
+    targets: '.ml5 .line',
     duration: 600,
     easing: "easeOutExpo",
     translateY: (el, i) => (-0.625 + 0.625*2*i) + "em"
   }).add({
-    targets: '.biganim .ampersand',
+    targets: '.ml5 .ampersand',
     opacity: [0,1],
     scaleY: [0.5, 1],
     easing: "easeOutExpo",
     duration: 600,
     offset: '-=600'
   }).add({
-    targets: '.biganim .letters-left',
+    targets: '.ml5 .letters-left',
     opacity: [0,1],
     translateX: ["0.5em", 0],
     easing: "easeOutExpo",
     duration: 600,
     offset: '-=300'
   }).add({
-    targets: '.biganim .letters-right',
+    targets: '.ml5 .letters-right',
     opacity: [0,1],
     translateX: ["-0.5em", 0],
     easing: "easeOutExpo",
     duration: 600,
     offset: '-=600'
   }).add({
-    targets: '.biganim',
+    targets: '.ml5',
     opacity: 0,
     duration: 1000,
     easing: "easeOutExpo",
